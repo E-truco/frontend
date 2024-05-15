655f174ad031ec541329034def0c158b8822e35e
icrcode <icarobotelhosocial@gmail.com>
2024-05-14 21:03:45 -0300

    jesus amado compilou

--- front_etruco/lib/main.dart
+++ front_etruco/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:front_etruco/pages/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'etruco.io',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
