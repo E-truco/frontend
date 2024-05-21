d7b7d2606de97c7beb9773cdced3570bfa7d907c
icrcode <icarobotelhosocial@gmail.com>
2024-05-21 20:35:56 -0300

    Agora sim temos uma pagina de Login!

--- front_etruco/lib/main.dart
+++ front_etruco/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_etruco/pages/login_screen.dart'; // Certifique-se de importar o arquivo correto
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(home: LoginScreen());
   }
 }
---
+++ front_etruco/lib/pages/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Usuário',
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Senha',
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              child: Text('Entrar'),
+              onPressed: () {
+
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
